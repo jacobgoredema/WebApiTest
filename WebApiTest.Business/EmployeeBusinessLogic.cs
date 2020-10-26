364090d6e42bc652bdc0173870d8e45c9644ab1f
Jay <Jay@DEV-PC>
2020-10-26 08:57:23 +0200

    Add project files.

---
+++ WebApiTest.Business/EmployeeBusinessLogic.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiTest.Data;
+using WebApiTest.Entities;
+
+namespace WebApiTest.Business
+{
+    public interface EmployeeBusinessLogic
+    {
+        IEnumerable<Employee> GetEmployeeList();
+        IEnumerable<Employee> GetEmployeeById(int id);
+        void UpdateEmployee(Employee employee);
+        void AddEmployee(Employee employee);
+        void DeleteEmployee(int id);
+
+    }
+
+    public class EmployeeBL : EmployeeBusinessLogic
+    {
+        WebApiTestEntities db = new WebApiTestEntities();
+
+        public void AddEmployee(Employee employee)
+        {
+            using (var db = new WebApiTestEntities())
+            {
+                Employee NewEmployee = new Employee();
+                NewEmployee.Name = employee.Name;
+                NewEmployee.Code = employee.Code;
+                NewEmployee.Salary = employee.Salary;
+
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            using (var db = new WebApiTestEntities())
+            {
+                Employee deleteEmployee = db.Employees.Where(x => x.EmployeeId == id).FirstOrDefault();
+                db.Employees.Remove(deleteEmployee);
+                db.SaveChanges();
+
+            }
+        }
+
+        public IEnumerable<Employee> GetEmployeeById(int id)
+        {
+            using (var db = new WebApiTestEntities())
+            {
+                var query = (from emp in db.Employees.Where(x => x.EmployeeId == id)
+                             select new Employee
+                             {
+                                 Name = emp.Name,
+                                 Code = emp.Code,
+                                 Salary = emp.Salary
+                             }).ToList();
+
+                return query;
+            }
+        }
+
+        public IEnumerable<Employee> GetEmployeeList()
+        {
+            using (var db = new WebApiTestEntities())
+            {
+                var query = (from emp in db.Employees
+                             select new Employee
+                             {
+                                 Name = emp.Name,
+                                 Code = emp.Code,
+                                 Salary = emp.Salary
+                             });
+
+                return query.ToList();
+            }
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            using (var db = new WebApiTestEntities())
+            {
+                Employee updateEmploye = db.Employees.Where(x => x.EmployeeId == employee.EmployeeId).FirstOrDefault();
+                updateEmploye.Name = employee.Name;
+                updateEmploye.Code = employee.Code;
+                updateEmploye.Salary = employee.Salary;
+
+                db.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ WebApiTest/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiTest.Business;
+using WebApiTest.Data;
+using WebApiTest.Entities;
+
+namespace WebApiTest.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        EmployeeBusinessLogic bl = new EmployeeBL();
+
+        // GET api/values
+        public IEnumerable<Employee> Get(int id)
+        {
+            return bl.GetEmployeeById(id);
+        }
+
+        // GET api/values/5
+        public IEnumerable<Employee> GetEmployeesList()
+        {
+            return bl.GetEmployeeList();
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
